57f3476209ccf04c0eb3a1c39c02ffcee1ec6ffd
editwithharsh4-spec <editwithharsh4@gmail.com>
2026-07-31 11:16:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-for (let i = 0; i < 20; i++) {
-  let b = document.createElement("div");
-  b.className = "balloon";
+const text =
+"Every heartbeat of mine whispers your name Chini ❤️";
 
-  b.style.left = Math.random() * 100 + "vw";
-  b.style.animationDuration = (6 + Math.random() * 5) + "s";
-  b.style.animationDelay = Math.random() * 3 + "s";
+let i = 0;
 
-  const colors = [
-    "#ff4d6d",
-    "#ff9f1c",
-    "#ffd60a",
-    "#4cd137",
-    "#00a8ff",
-    "#9c6ade",
-    "#ff6bcb"
-  ];
-
-  b.style.background = colors[Math.floor(Math.random() * colors.length)];
-
-  document.body.appendChild(b);
+function type() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 60);
+  }
 }
 
 setTimeout(() => {
-  alert("❤️ Happy Girlfriend Day My Chini ❤️");
-}, 1500);
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  type();
+}, 3000);
+
+document.getElementById("btn").onclick = () => {
+  document.getElementById("letter").style.display = "block";
+
+  if (typeof confetti === "function") {
+    confetti({
+      particleCount: 250,
+      spread: 180,
+      origin: { y: 0.6 }
+    });
+  }
+};
